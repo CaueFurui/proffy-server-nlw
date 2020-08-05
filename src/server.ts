3f80782f990daed1592908e948b8eed7da063155
CaueFurui <cauefurui@gmail.com>
2020-08-04 21:08:16 -0300

    [KNEX&SQLITE]Adding sqlite and knex migrations

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(3333)
